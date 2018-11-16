6c2005f0932447e4ee51b66100a29b0c738ad7e7
1660293 <1660293@student.hcmus.edu.vn>
2018-11-16 21:17:18 +0700

    make code edit

--- ADONET-3-Layers_GiaoDien/Form1.cs
+++ ADONET-3-Layers_GiaoDien/Form1.cs
@@ -16,24 +16,39 @@ namespace ADONET_3_Layers_GiaoDien
     {
         private int originalW;
         private int originalX;
+        
         public Form1()
         {
             InitializeComponent();
+            
+            
             loadSuppliersToComboBox();
             loadCategoriesToComboBox();
             loadListOfProductToDataGridView();
+            dgvListOfProducts.CellClick += new DataGridViewCellEventHandler(dgvListOfProducts_CellClick);
+            dgvListOfProducts.CellContentClick += new DataGridViewCellEventHandler(dgvListOfProducts_CellContentClick);
+        }
+        private void dgvListOfProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            showSelectedRowDataForm(e.RowIndex);
+        }
+        private void dgvListOfProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            showSelectedRowDataForm(e.RowIndex);
         }
         // Support Functions
         private void loadListOfProductToDataGridView()
         {
             dgvListOfProducts.DataSource = Handler.getListOfProducts();
+            dgvListOfProducts.Columns["ProductID"].ReadOnly = true;
+            if (dgvListOfProducts.RowCount > 0) showSelectedRowDataForm(0);
         }
         private void loadSuppliersToComboBox()
         {
             List<Tuple<int, string>> suppliers = Handler.getListOfSupplier();
-            if(suppliers != null && suppliers.Count > 0)
+            if (suppliers != null && suppliers.Count > 0)
             {
-               
+
                 foreach (Tuple<int, string> i in suppliers)
                 {
                     cbSupplier.Items.Add(i.Item2);
@@ -51,7 +66,7 @@ namespace ADONET_3_Layers_GiaoDien
 
                 foreach (Tuple<int, string> i in categories)
                 {
-                    cbCategory.Items.Add( i.Item2);
+                    cbCategory.Items.Add(i.Item2);
                     //cbCategory.Items.Add(i.Item1 + ". " + i.Item2);
                 }
                 //cbCategory.SelectedIndex = 0;
@@ -83,6 +98,40 @@ namespace ADONET_3_Layers_GiaoDien
         private void btnEdit_Click_1(object sender, EventArgs e)
         {
             rbEdit.Checked = true;
+
+
+            int newid = parseInt(txtId.Text);
+            string name = txtName.Text;
+            int supplierID = cbSupplier.SelectedIndex + 1;
+            int categoryID = cbCategory.SelectedIndex + 1;
+            string quantity = txtQuantityPerUnit.Text;
+            float price = parseFloat(txtUnitPrice.Text);
+            int stock = parseInt(txtUnitsInStock.Text);
+            int order = parseInt(txtUnitsOnOrder.Text);
+            int reorder = parseInt(txtReoderLevel.Text);
+            bool discontinued = parseIntToBool(cbDiscontinued.SelectedIndex);
+            DialogResult res = MessageBox.Show("Do you really want Edit?", "Wait", MessageBoxButtons.YesNo);
+            if (res == DialogResult.Yes)
+            {
+                Handler.UpdateProducts(newid, name, supplierID, categoryID, quantity, price, stock, order, reorder, discontinued);
+                loadListOfProductToDataGridView();
+            }
+        }
+        private void showSelectedRowDataForm(int index)
+        {
+            if (index >= 0)
+            {
+                txtId.Text = dgvListOfProducts.Rows[index].Cells[0].Value.ToString();
+                txtName.Text = dgvListOfProducts.Rows[index].Cells[1].Value.ToString();
+                cbSupplier.SelectedIndex = parseInt(dgvListOfProducts.Rows[index].Cells[2].Value.ToString()) - 1;
+                cbCategory.SelectedIndex = parseInt(dgvListOfProducts.Rows[index].Cells[3].Value.ToString()) - 1;
+                txtQuantityPerUnit.Text = dgvListOfProducts.Rows[index].Cells[4].Value.ToString();
+                txtUnitPrice.Text = dgvListOfProducts.Rows[index].Cells[5].Value.ToString();
+                txtUnitsInStock.Text = dgvListOfProducts.Rows[index].Cells[6].Value.ToString();
+                txtUnitsOnOrder.Text = dgvListOfProducts.Rows[index].Cells[7].Value.ToString();
+                txtReoderLevel.Text = dgvListOfProducts.Rows[index].Cells[8].Value.ToString();
+                cbDiscontinued.SelectedIndex = parseInt(dgvListOfProducts.Rows[index].Cells[9].Value.ToString());
+            }
         }
 
         private void btnDelete_Click_1(object sender, EventArgs e)
@@ -90,7 +139,7 @@ namespace ADONET_3_Layers_GiaoDien
             rbDelete.Checked = true;
             if (dgvListOfProducts.SelectedRows.Count > 0)
             {
-                DialogResult res= MessageBox.Show("Do you really want delete?", "Wait", MessageBoxButtons.YesNo);
+                DialogResult res = MessageBox.Show("Do you really want delete?", "Wait", MessageBoxButtons.YesNo);
                 if (res == DialogResult.Yes)
                 {
                     string id = dgvListOfProducts.Rows[dgvListOfProducts.CurrentRow.Index].Cells[0].Value.ToString();
@@ -107,6 +156,34 @@ namespace ADONET_3_Layers_GiaoDien
             {
                 MessageBox.Show("Please select 1 row before delete", "Warning");
             }
+        }
+        private int parseInt(string str)
+        {
+            int result = 0;
+            if (str == "True")
+            {
+                return 1;
+            }
+            else if (str == "False") return 0;
+            if (!Int32.TryParse(str, out result)) result = 0;
+            return result;
+        }
+        private float parseFloat(string str)
+        {
+            float result = 0;
+            if (!float.TryParse(str, out result)) result = 0;
+            return result;
+        }
+        private Boolean parseBoolean(string str)
+        {
+            Boolean result = true;
+            if (!bool.TryParse(str, out result)) result = false;
+            return result;
+        }
+        private bool parseIntToBool(int i)
+        {
+            if (i == 1) return true;
+            return false;
         }
     }
 }
--- ADONET-3-Layers_XuLy/Handler.cs
+++ ADONET-3-Layers_XuLy/Handler.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ADONET_3_Layers_KetNoi;
+using System.Data.SqlClient;
 
 namespace ADONET_3_Layers_XuLy
 {
@@ -52,6 +53,30 @@ namespace ADONET_3_Layers_XuLy
                 categories.Add(new Tuple<int, string>((int)row["CategoryID"], row["CategoryName"].ToString()));
             }
             return categories;
+        }
+        public static bool UpdateProducts(int id, string name, int supplier, int category, string quantity, float price, int stock, int order, int reorder, bool discontinued)
+        {
+            DataProvider.Connect();
+            if ((DataProvider.Select(CommandType.Text, "SELECT * FROM Categories where CategoryID = " + category).Rows.Count == 0) || (DataProvider.Select(CommandType.Text, "SELECT * FROM Suppliers where SupplierID = " + supplier).Rows.Count == 0))
+            {
+                DataProvider.Disconnect();
+                return false;
+            }
+            string query = "UPDATE [Products] SET [ProductName] = @name,[SupplierID] =@supplier,[CategoryID] =@category,[QuantityPerUnit] =@quantity,[UnitPrice] =@price,[UnitsInStock] = @stock,[UnitsOnOrder] =@order,[ReorderLevel] =@reorder,[Discontinued] =@discontinued where [ProductID] = @oldid";
+            DataProvider.ExecuteNonQuery(CommandType.Text, query
+           , new SqlParameter { ParameterName = "@name", Value = name }
+           , new SqlParameter { ParameterName = "@supplier", Value = supplier }
+           , new SqlParameter { ParameterName = "@category", Value = category }
+           , new SqlParameter { ParameterName = "@quantity", Value = quantity }
+           , new SqlParameter { ParameterName = "@price", Value = price }
+           , new SqlParameter { ParameterName = "@stock", Value = stock }
+           , new SqlParameter { ParameterName = "@order", Value = order }
+           , new SqlParameter { ParameterName = "@reorder", Value = reorder }
+           , new SqlParameter { ParameterName = "@discontinued", Value = discontinued }
+           , new SqlParameter { ParameterName = "@oldid", Value = id }
+           );
+            DataProvider.Disconnect();
+            return true;
         }
     }
 }
